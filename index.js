dad4bd7a55178a37e5ec7910bf870c0dd5c80170
icykirby <jamericanmadison@gmail.com>
2025-03-12 02:26:32 -0400

    added a welcome sign and made the transition for the login button

---
+++ index.js
@@ -0,0 +1,6 @@
+document.getElementById("getStarted").addEventListener("click", function(){
+    const div = document.getElementById("loginContainer");
+    div.classList.add("visible");
+    div.style.transform = "translateX(700px)";
+    
+});
