0dc0d316b8b81cdc10c2a1d26e8b5847d71c7fe5
Daniel <danielram2023@gmail.com>
2025-03-12 05:26:15 -0400

    add register form

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
-document.getElementById("getStarted").addEventListener("click", function(){
-    const div = document.getElementById("loginContainer");
-    div.classList.add("visible");
-    div.style.transform = "translateX(350px)";
-    
+document.getElementById("log").addEventListener("click", function () {
+  const loginDiv = document.getElementById("loginContainer");
+  const registerDiv = document.getElementById("registerContainer");
+
+  // Hide register container
+  registerDiv.classList.remove("visible");
+  registerDiv.style.transform = "translateX(0px)";
+
+  // Show login container
+  loginDiv.classList.add("visible");
+  loginDiv.style.transform = "translateX(350px)";
 });
 
-document.getElementById("loginBtn").addEventListener("click", function(e){
-    window.location.href = "mainPage.html";
+document.getElementById("reg").addEventListener("click", function () {
+  const loginDiv = document.getElementById("loginContainer");
+  const registerDiv = document.getElementById("registerContainer");
+
+  // Hide login container
+  loginDiv.classList.remove("visible");
+  loginDiv.style.transform = "translateX(0px)";
+
+  // Show register container
+  registerDiv.classList.add("visible");
+  registerDiv.style.transform = "translateX(350px)";
+});
+
+document.getElementById("loginBtn").addEventListener("click", function (e) {
+  window.location.href = "mainPage.html";
 });
